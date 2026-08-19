5b19c3969dfa97b5fa7c331f8866f25436dc67fb
dayan <daniyar.sharshenbaev@energobank.kg>
2026-08-19 14:58:59 +0600

    netswitch 0.1.3
    Colorized init output, Enter defaults to best candidate

--- Formula/netswitch.rb
+++ Formula/netswitch.rb
@@ -1,10 +1,10 @@
 class Netswitch < Formula
   desc "Toggle between internal and external network on macOS (USB-LAN + squid)"
   homepage "https://github.com/bayoux/homebrew-netswitch"
-  url "https://github.com/bayoux/homebrew-netswitch/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "75932c4c7612cf17c2876c988b38b1c9ff9c290b08cedd08cafc4c50365e2d4b"
+  url "https://github.com/bayoux/homebrew-netswitch/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "f3d1ef96ca7d28dc86a703316b5077691602cc0dfe2f39155a9aff84397e63fb"
   license "MIT"
-  version "0.1.2"
+  version "0.1.3"
 
   def install
     bin.install "netswitch"
